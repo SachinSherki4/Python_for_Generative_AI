5c249edcd58c6aeb88e3ab194d06cb97b4453eec
Sachin Sherki <ssherki@gamil.com>
2025-04-28 17:34:19 +0530

    adding gitignore file

--- Python_DSA/List/list_exercise.py
+++ Python_DSA/List/list_exercise.py
@@ -21,6 +21,14 @@ alfa_num=["sachin",5,1,"sam","jack",7]
 #print(sorted(alfa_num)) # TypeError
 print(fruits.reverse())
 
+# List momery allocations : here both list_1 and list_2 pointing same memory locations
+# so any change in one list will reflect in both list
+list_1=[1,2,3]
+list_2=list_1
+list_1[0]=9
+list_2.append(4)
+print(list_1,list_2) #[9, 2, 3, 4] [9, 2, 3, 4]
+
 # use cases
 #1. Store predictions from ML models
 predictions = [0.9, 0.8, 0.75, 0.6]
